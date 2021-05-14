772d56a99e614e797925dcae1ffcff0f552643fb
Andres Campuzano Garzon <andres.campuzano@evo-ion.com>
2021-05-13 21:38:20 -0500

    fix(index): lefnavbarsmall import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { LeftNavBarBig } from "../components/Layout/LeftNavbar/LeftNavBarBig"
-import { LeftNavBarSmall } from "../components/Layout/LeftNavbar/LeftNavbarSmall"
+import { LeftNavBarSmall } from "../components/Layout/LeftNavbar/LeftNavBarSmall"
 import { PageManager } from "../components/Layout/PageManager"
 import { ScrollContainer } from "../components/Layout/ScrollContainer"
 import { TopNavbar } from "../components/Layout/TopNavbar"
